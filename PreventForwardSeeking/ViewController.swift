3712eab175824542afd141657749dbd3c10cd0a8
David Cordero <david@corderoramirez.com>
2021-08-05 13:50:52 +0200

    Initial Commit

---
+++ PreventForwardSeeking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreventForwardSeeking
+//
+//  Created by David Cordero on 05.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
